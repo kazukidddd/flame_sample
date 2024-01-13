c69e69e0405232f81b4ae7b8436a5ca046dda835
kazuki_kimura <kazukidddd836@gmail.com>
2024-01-14 07:44:17 +0900

    Refactor game initialization and widget structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flame_sample/src/widgets/game_app.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const GameApp());
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
 }
